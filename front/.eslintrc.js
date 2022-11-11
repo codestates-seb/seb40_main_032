b963f7b63e0f0acfd9cad3a2d5a9788cb1242d42
kuku <covernat6949@gmail.com>
2022-11-11 15:39:07 +0900

    [Style] Airbnb eslint 적용

---
+++ front/.eslintrc.js
@@ -0,0 +1,41 @@
+module.exports = {
+  env: {
+    browser: true,
+    es6: true,
+    node: true,
+  },
+  extends: [
+    'airbnb',
+    'prettier',
+    'eslint:recommended',
+    'plugin:prettier/recommended',
+  ],
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: 'module',
+  },
+  rules: {
+    'no-alert': 'off',
+    'import/no-unresolved': 'off',
+    'react/button-has-type': 'off',
+    'react/react-in-jsx-scope': 'off',
+    'react/prop-types': 0,
+    'no-param-reassign': [
+      'error',
+      { props: true, ignorePropertyModificationsFor: ['state'] },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      2,
+      {
+        labelAttributes: ['htmlFor'],
+      },
+    ],
+    'react/jsx-filename-extension': [
+      'error',
+      {
+        extensions: ['.js', '.jsx'],
+      },
+    ],
+    'no-console': 0,
+  },
+};
