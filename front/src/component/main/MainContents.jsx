17c081bf17b3a508bb1cb039bfcbe5256e061704
seungmin2ee <tmdals2815@gmail.com>
2022-11-16 02:00:39 +0900

    [Feat] MainContents 미디어쿼리 적용

--- front/src/component/main/MainContents.jsx
+++ front/src/component/main/MainContents.jsx
@@ -7,10 +7,32 @@ const MainContainer = styled.section`
   padding: 0 calc(10rem - 1.2rem);
   position: relative;
 
+  @media screen and (max-width: 549px) {
+    padding: 0;
+  }
+
   .main__container {
     max-width: 172rem;
     display: flex;
     flex-wrap: wrap;
+
+    @media screen and (max-width: 1440px) {
+      .post {
+        flex-basis: 33.3%;
+      }
+    }
+
+    @media screen and (max-width: 1024px) {
+      .post {
+        flex-basis: 50%;
+      }
+    }
+
+    @media screen and (max-width: 549px) {
+      .post {
+        flex-basis: 100%;
+      }
+    }
   }
 `;
 
