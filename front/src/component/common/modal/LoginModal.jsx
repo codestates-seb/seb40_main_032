f2dc74cd3fcc2bd30e9a728db9fecf8e6b57be65
starplanter93 <starplanter93@gmail.com>
2022-11-16 15:55:23 +0900

    [Feat] 로그인모달에서 회원가입 모달로 이동하는 기능 구현

--- front/src/component/common/modal/LoginModal.jsx
+++ front/src/component/common/modal/LoginModal.jsx
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 import ModalCard from './ModalCard';
 import Backdrop from './Backdrop';
 import { DefaultButton, TransparentButton } from '../button/ButtonStyle';
+import SignupModal from './SignupModal';
 
 const LoginModalStyle = styled.div`
   display: flex;
@@ -58,13 +59,22 @@ const LoginModalStyle = styled.div`
 function LoginModal({ modalCloser }) {
   const [email, setEmail] = useState('');
   const [password, setPassword] = useState('');
+  const [signupModalOpened, setSignupModalOpened] = useState(false);
 
+  // 인풋값 상태 저장 함수
   const onChangeEmail = e => {
     setEmail(e.target.value);
   };
-
   const onChangePassword = e => {
     setPassword(e.target.value);
+  };
+
+  // 회원가입 버튼 클릭 시 회원가입 모달을 띄우기 위한 상태 변경 함수
+  const signupModalOpener = () => {
+    setSignupModalOpened(true);
+  };
+  const signupModalCloser = () => {
+    setSignupModalOpened(false);
   };
 
   // 추후 로그인 axios를 onSubmitHandler 함수에 작성할 것.
@@ -75,54 +85,57 @@ function LoginModal({ modalCloser }) {
   };
 
   return (
-    <Backdrop onClick={modalCloser}>
-      <ModalCard
-        onStopPropagation={e => {
-          e.stopPropagation();
-        }}
-        onClick={modalCloser}
-      >
-        <LoginModalStyle>
-          <div className="title">로그인</div>
-          <form className="login__form">
-            <label htmlFor="email">아이디</label>
-            <input
-              id="email"
-              name="email"
-              type="text"
-              placeholder="이메일 주소를 입력하세요"
-              onChange={onChangeEmail}
-              value={email}
-            />
-            <label htmlFor="password">비밀번호</label>
-            <input
-              id="password"
-              name="password"
-              type="password"
-              placeholder="비밀번호를 입력하세요"
-              onChange={onChangePassword}
-              value={password}
-            />
-            <DefaultButton
-              width="100%"
-              height="4rem"
-              fontSize="1.7rem"
-              onClick={onSubmitHandler}
-              type="submit"
-              margin="1.5rem 0"
-            >
-              로그인
-            </DefaultButton>
-          </form>
-          <footer className="footer">
-            <div className="footer__content">아직 회원이 아니신가요?</div>
-            <TransparentButton fontSize="1.4rem">
-              회원가입 &gt;
-            </TransparentButton>
-          </footer>
-        </LoginModalStyle>
-      </ModalCard>
-    </Backdrop>
+    <>
+      <Backdrop onClick={modalCloser}>
+        <ModalCard
+          onStopPropagation={e => {
+            e.stopPropagation();
+          }}
+          onClick={modalCloser}
+        >
+          <LoginModalStyle>
+            <div className="title">로그인</div>
+            <form className="login__form">
+              <label htmlFor="email">아이디</label>
+              <input
+                id="email"
+                name="email"
+                type="text"
+                placeholder="이메일 주소를 입력하세요"
+                onChange={onChangeEmail}
+                value={email}
+              />
+              <label htmlFor="password">비밀번호</label>
+              <input
+                id="password"
+                name="password"
+                type="password"
+                placeholder="비밀번호를 입력하세요"
+                onChange={onChangePassword}
+                value={password}
+              />
+              <DefaultButton
+                width="100%"
+                height="4rem"
+                fontSize="1.7rem"
+                onClick={onSubmitHandler}
+                type="submit"
+                margin="1.5rem 0"
+              >
+                로그인
+              </DefaultButton>
+            </form>
+            <footer className="footer">
+              <div className="footer__content">아직 회원이 아니신가요?</div>
+              <TransparentButton fontSize="1.4rem" onClick={signupModalOpener}>
+                회원가입 &gt;
+              </TransparentButton>
+            </footer>
+          </LoginModalStyle>
+        </ModalCard>
+      </Backdrop>
+      {signupModalOpened && <SignupModal modalCloser={signupModalCloser} />}
+    </>
   );
 }
 
