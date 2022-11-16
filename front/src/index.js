97be3f0de2f976a9965f1a1806bd666e286e626c
kuku <covernat6949@gmail.com>
2022-11-16 18:57:52 +0900

    [Feat] store 생성 및 초기세팅

--- front/src/index.js
+++ front/src/index.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 import App from './App';
 import GlobalStyle from './assets/GlobalStyle';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <GlobalStyle />
-    <App />
+    <Provider store={store}>
+      <GlobalStyle />
+      <App />
+    </Provider>
   </React.StrictMode>,
 );
