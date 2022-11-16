6d253848af3889380c39fdce150903e77c0b60fe
DT-KUKU <107905739+DT-KUKU@users.noreply.github.com>
2022-11-16 13:41:54 +0900

    [Feat] 헤더 컴포넌트 작업 및 버튼 옵션 추가 #70(#74)
    * [Feat] 버튼 font-weight 옵션 추가
    
    * [Feat] Search 컴포넌트 작업 및 반응형 적용
    
    * [Feat] 로그인 버튼 및 유저 정보 컴포넌트 작업 및 반응형 적용
    
    * [Feat] 헤더 통합 및 반응형 적용
    
    * [Feat] 중복코드 및 bottom 색 수정
    
    * [Feat] 중복코드 수정

---
+++ front/src/component/common/header/HeaderUser.jsx
@@ -0,0 +1,100 @@
+import styled from 'styled-components';
+import { useState } from 'react';
+import { GiHamburgerMenu } from 'react-icons/gi';
+import { RiArrowDownSFill } from 'react-icons/ri';
+import { BiUserCircle } from 'react-icons/bi';
+import { DefaultButton } from '../button/ButtonStyle';
+
+const HeaderUserWrapper = styled.nav`
+  width: 100%;
+  height: 100%;
+  display: flex;
+  align-items: center;
+  min-width: 20rem;
+  .header__write {
+    margin-left: 0.8rem;
+    margin-right: 0.8rem;
+    color: var(--font-base-black);
+    background-color: transparent;
+    border: 1px solid transparent;
+    font-size: 2rem;
+    cursor: pointer;
+    flex: 1;
+  }
+  .header__box {
+    display: flex;
+    align-items: center;
+    cursor: pointer;
+    > :last-child {
+      transition: transform 300ms;
+      transform: rotate(360deg);
+    }
+  }
+  .header__box--rorate {
+    > :last-child {
+      transition: transform 300ms;
+      transform: rotate(180deg);
+    }
+  }
+  .header__burger {
+    display: none;
+  }
+
+  @media screen and (max-width: 549px) {
+    display: flex;
+    justify-content: right;
+    min-width: 0px;
+    cursor: pointer;
+    .header__write {
+      display: none;
+    }
+    .header__box {
+      display: none;
+    }
+    .header__burger {
+      display: block;
+    }
+  }
+`;
+
+function HeaderUser() {
+  const [rorate, setRorate] = useState('');
+  const [isLogin, setIsLogin] = useState(false);
+
+  const loginHandler = () => {
+    setIsLogin(true);
+  };
+
+  return (
+    <HeaderUserWrapper>
+      <button className="header__write">게시물 작성</button>
+      <div
+        className={`header__box ${isLogin ? `header__box${rorate}` : ''}`}
+        role="button"
+        tabIndex={0}
+        onClick={() => setRorate(prev => (prev !== '' ? '' : '--rorate'))}
+        onKeyDown={() => setRorate(prev => (prev !== '' ? '' : '--rorate'))}
+      >
+        {!isLogin ? (
+          <DefaultButton
+            onClick={() => loginHandler()}
+            fontSize="2rem"
+            width="10rem"
+            height="5rem"
+            fontWeight="700"
+          >
+            로그인
+          </DefaultButton>
+        ) : (
+          <>
+            <BiUserCircle size="4.3rem" />
+            <RiArrowDownSFill size="2.5rem" color="hsl(146, 50%, 50%)" />
+          </>
+        )}
+      </div>
+      <GiHamburgerMenu className="header__burger" size="3.4rem" />
+    </HeaderUserWrapper>
+  );
+}
+
+export default HeaderUser;
