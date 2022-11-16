a2a026737390f74f25b132999317fe2e30884f97
Cutaku <107826896+Cutaku@users.noreply.github.com>
2022-11-16 20:12:48 +0900

    댓글 수정 API 구현 및 테스트 #62(#93)
    * [Feat] 댓글 수정 API 구현
    
    * [Test] 댓글 수정 API 테스트

--- back/src/main/java/travelRepo/domain/comment/repository/CommentRepository.java
+++ back/src/main/java/travelRepo/domain/comment/repository/CommentRepository.java
@@ -1,10 +1,13 @@
 package travelRepo.domain.comment.repository;
 
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 import travelRepo.domain.comment.entity.Comment;
+
+import java.util.Optional;
 
 public interface CommentRepository extends JpaRepository<Comment, Long> {
 
@@ -16,4 +19,9 @@ public interface CommentRepository extends JpaRepository<Comment, Long> {
     @Modifying(clearAutomatically = true, flushAutomatically = true)
     @Query("delete from Comment comment where comment.board.id = :boardId")
     void deleteByBoardId(@Param("boardId") Long boardId);
+
+    @EntityGraph(attributePaths = {"account"})
+    @Query("select c from Comment c where c.id = :commentId")
+    Optional<Comment> findByIdWithAccount(@Param("commentId") Long commentId);
+
 }
