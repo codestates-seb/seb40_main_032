7791e6b7c661157ff4a15e79cd2f219199edf6b7
Cutaku <107826896+Cutaku@users.noreply.github.com>
2022-11-16 13:43:30 +0900

    [Test] 테스트 데이터 추가

--- back/src/test/resources/static/db/data.sql
+++ back/src/test/resources/static/db/data.sql
@@ -9,6 +9,7 @@ INSERT INTO Follow(follow_id, createdAt, modifiedAt, follower_id, following_id) 
 INSERT INTO Board(board_id, createdAt, modifiedAt, category, content, likeCount, location, title, views, account_id) VALUES('12001', '2022-10-14 12:00:01', '2022-10-14 12:00:01', 'RESTAURANT', 'testContents', '1', 'test-location', 'testTitle', '10', '10001');
 INSERT INTO Board(board_id, createdAt, modifiedAt, category, content, likeCount, location, title, views, account_id) VALUES('12002', '2022-10-14 12:00:02', '2022-10-14 12:00:02', 'SPOT', 'testContents', '3', 'test-location', 'testTitle', '10', '10002');
 INSERT INTO Board(board_id, createdAt, modifiedAt, category, content, likeCount, location, title, views, account_id) VALUES('12003', '2022-10-14 12:00:03', '2022-10-14 12:00:03', 'STAY', 'testContents', '1', 'test-location', 'testTitle', '10', '10003');
+INSERT INTO Board(board_id, createdAt, modifiedAt, category, content, likeCount, location, title, views, account_id) VALUES('12004', '2022-10-14 12:00:04', '2022-10-14 12:00:04', 'STAY', 'testContents', '1', 'test-location', 'testTitle', '10', '10001');
 
 INSERT INTO BoardPhoto(boardPhoto_id, board_id, photo) VALUES('13001', '12001', 'https://main-image-repo.s3.ap-northeast-2.amazonaws.com/39c10d6d-2765-479d-a45f-662e619fd006.jpeg');
 INSERT INTO BoardPhoto(boardPhoto_id, board_id, photo) VALUES('13002', '12001', 'https://main-image-repo.s3.ap-northeast-2.amazonaws.com/39c10d6d-2765-479d-a45f-662e619fd006.jpeg');
