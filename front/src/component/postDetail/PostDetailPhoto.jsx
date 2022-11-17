9f21211f751447102e83980aa98b6a30544c409e
Seungmok <103170697+yoosmg@users.noreply.github.com>
2022-11-17 21:31:28 +0900

    [Feat] 게시글 상세화면 본문 컴포넌트 레이아웃/스타일 적용 (#110)
    * [Feat] 게시글 상세화면 본문 컴포넌트 레이아웃/스타일 적용
    
    * [Style] 게시글 상세화면 페이지 추가
    
    * [Fix] ment 색상 적용 방식 수정 및 글자와의 간격 조절 기능 추가

---
+++ front/src/component/postDetail/PostDetailPhoto.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import styled from 'styled-components';
+import PostMockData from './PostMockData';
+
+const Container = styled.div`
+  width: 42vw;
+  height: 70vh;
+  object-fit: cover;
+  @media screen and (max-width: 549px) {
+    width: 88vw;
+    height: 32rem;
+  }
+`;
+
+const TempImg = styled.img`
+  width: inherit;
+  height: inherit;
+  object-fit: cover;
+  border: 1px solid var(--holder-base-color);
+  border-right: none;
+  @media screen and (max-width: 549px) {
+    border: 1px solid var(--holder-base-color);
+    border-bottom: none;
+  }
+`;
+
+function PostDetailPhoto() {
+  const mockData = PostMockData;
+  // 임시 구현 페이지입니다.
+
+  return (
+    <Container>
+      <TempImg src={mockData.photos[0]} />
+    </Container>
+  );
+}
+
+export default PostDetailPhoto;
