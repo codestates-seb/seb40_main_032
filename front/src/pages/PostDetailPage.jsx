a3c6afa4d44c408656b69df6a75fe9cdb547a110
yoosmg <sryu2s@gmail.com>
2022-11-17 19:18:00 +0900

    [Style] 게시글 상세화면 페이지 추가

---
+++ front/src/pages/PostDetailPage.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import PostDetailMain from '../component/postDetail/PostDetailMain';
+
+function PostDetailPage() {
+  return <PostDetailMain />;
+}
+
+export default PostDetailPage;
