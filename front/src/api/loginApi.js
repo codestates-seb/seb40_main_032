105e3947b88540bd0ec33b91d2b281562b0cb450
starplanter93 <starplanter93@gmail.com>
2022-11-17 22:37:15 +0900

    [Feat] 로그인 axios 요청 작성

--- front/src/api/loginApi.js
+++ front/src/api/loginApi.js
@@ -1,14 +1,7 @@
 import axios from 'axios';
-// 초기 세팅입니다. url data 전부 변경하시면 됩니다.
-const loginApi = (url, data) => {
-  return axios
-    .post(url, data)
-    .then(res => {
-      return res;
-    })
-    .catch(err => {
-      return err;
-    });
+
+const loginApi = data => {
+  return axios.post('/login', data);
 };
 
 export default loginApi;
