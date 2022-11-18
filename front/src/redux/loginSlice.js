a95fec81042b76140f11bee62d3706f377e6668d
starplanter93 <starplanter93@gmail.com>
2022-11-18 12:38:12 +0900

    [Feat] loginSlice reducer까지 작성

--- front/src/redux/loginSlice.js
+++ front/src/redux/loginSlice.js
@@ -1,12 +1,21 @@
 import { createSlice } from '@reduxjs/toolkit';
 import loginAsync from '../action/loginAsync';
+import { getCookie } from '../util/cookie';
 
+const accessToken = getCookie('accessToken');
 const loginSlice = createSlice({
   name: 'login',
   initialState: {
-    isLogin: false,
+    isLogin: !!accessToken,
   },
-  reducers: {},
+  reducers: {
+    login(state) {
+      state.isLogin = true;
+    },
+    logout(state) {
+      state.isLogin = false;
+    },
+  },
   extraReducers: {
     [loginAsync.pending]: (state, action) => {
       console.log(state, action);
