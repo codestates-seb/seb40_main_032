e4de345312d19eb9b3f48711abbc907098771914
starplanter93 <starplanter93@gmail.com>
2022-11-18 17:06:54 +0900

    [Layout] App.js에 헤더 추가

--- front/src/App.js
+++ front/src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 import React, { Suspense } from 'react';
 import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
 import Loading from './component/common/Loading';
+import Header from './component/common/header/Header';
 
 const MainPage = React.lazy(() => import('./pages/MainPage'));
 const PublishPage = React.lazy(() => import('./pages/PublishPage'));
@@ -11,6 +12,7 @@ function App() {
   return (
     <Router>
       <Suspense fallback={<Loading />}>
+        <Header />
         <Routes>
           <Route path="/" element={<MainPage />} />
           <Route path="/postDetail/:id" element={<PostDetailPage />} />
