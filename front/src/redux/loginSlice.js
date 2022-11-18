cb37da2e4aa5e5b90d24dd33db3a0826bc477c6d
starplanter93 <starplanter93@gmail.com>
2022-11-18 16:03:23 +0900

    [Feat] loginSlice 작성

--- front/src/redux/loginSlice.js
+++ front/src/redux/loginSlice.js
@@ -1,12 +1,13 @@
 import { createSlice } from '@reduxjs/toolkit';
 import loginAsync from './action/loginAsync';
-import { getCookie } from '../util/cookie';
+import { setCookie, getCookie } from '../util/cookie';
 
 const accessToken = getCookie('accessToken');
 const loginSlice = createSlice({
   name: 'login',
   initialState: {
     isLogin: !!accessToken,
+    accessToken,
   },
   reducers: {
     login(state) {
@@ -21,6 +22,17 @@ const loginSlice = createSlice({
       console.log(state, action);
     },
     [loginAsync.fulfilled]: (state, action) => {
+      state.isLogin = true;
+      // state.accessToken = action.payload.headers.authorization;
+      // 쿠키의 domain 옵션은 로컬호스트나 ip주소같은 url에선 작동하지 않음.
+      // chrome 정책에 의해 .com과 같은 일반적인 도메인 주소에서만 사용 가능.
+      // httpOnly 옵션을 설정할 경우 클라이언트측에서 쿠키를 저장, 사용할 수 없음.
+      // sameSite 옵션은 서버와 클라이언트의 주소가 다르므로 None으로 설정했음.
+      setCookie('accessToken', action.payload.headers.authorization, {
+        path: '/',
+        sameSite: 'None',
+        secure: 'false',
+      });
       console.log(state, action);
     },
     [loginAsync.rejected]: (state, action) => {
