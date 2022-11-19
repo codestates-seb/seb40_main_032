db98f821e53439db7ebe7af2d558785bb68b0555
yoosmg <sryu2s@gmail.com>
2022-11-19 22:53:50 +0900

    [Feat] 하단 버튼 모달 연결 및 구조 수정

--- front/src/component/publish/PublishForm.jsx
+++ front/src/component/publish/PublishForm.jsx
@@ -1,11 +1,13 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { useNavigate } from 'react-router-dom';
 import categoryData from './categoryData';
 import { DefaultButton, TransparentButton } from '../common/button/ButtonStyle';
 import PublishPhoto from './PublishPhoto';
+import ConfirmModal from '../common/modal/ConfirmModal';
+import YesNoModal from '../common/modal/YesNoModal';
 
-// 컨테이너
+// 전체 컨테이너
 const Container = styled.div`
   z-index: 5;
   display: flex;
@@ -54,8 +56,6 @@ const TitleContainer = styled.section`
     flex-direction: row;
     justify-content: space-between;
   }
-  > .title__input {
-  }
 `;
 
 // 본문 - 입력(필수)
@@ -92,7 +92,7 @@ const LengthMin = styled.span`
   color: ${props => (props.isValid ? 'var(--button-theme)' : 'red')};
 `;
 
-// 위치 - 입력
+// 위치/테마 열
 const LocationCategoryRow = styled.section`
   display: flex;
   justify-content: space-between;
@@ -103,25 +103,25 @@ const LocationCategoryRow = styled.section`
   }
 `;
 
+// 위치 - 입력(선택)
 const LocationContainer = styled.section`
   width: 100%;
 `;
 
-// 카테고리 - 선택(필수:택1)
+// 테마 - 선택(필수:택1)
 const CategoryContainer = styled.section`
   width: 100%;
+  #category {
+    display: flex;
+    flex-direction: row;
+    justify-content: space-around;
+    border: 1px solid var(--holder-base-color);
+    border-radius: var(--radius-10);
+    padding: 0.5rem;
+  }
   @media screen and (max-width: 549px) {
     width: 70%;
   }
-`;
-
-const CategoryWrapper = styled.div`
-  display: flex;
-  flex-direction: row;
-  justify-content: space-around;
-  border: 1px solid var(--holder-base-color);
-  border-radius: var(--radius-10);
-  padding: 0.5rem;
 `;
 
 const Category = styled.button`
@@ -147,43 +147,43 @@ const Category = styled.button`
 // 태그 - 입력, 삭제
 const TagContainer = styled.section`
   width: auto;
-`;
-
-const TagWrapper = styled.ul`
-  display: flex;
-  flex-direction: row;
-  max-width: 100%;
-  height: auto;
-  overflow: hidden;
-  border-radius: var(--radius-10);
-  border: 1px solid var(--holder-base-color);
-  flex-wrap: wrap;
-  .tag {
+  .tag__wrapper {
     display: flex;
-    align-items: center;
-    white-space: nowrap;
-    margin: 0 0.5rem 0 1rem;
+    flex-direction: row;
+    max-width: 100%;
+    height: auto;
+    overflow: hidden;
     border-radius: var(--radius-10);
-    color: var(--font-base-black);
-    font-size: 1.5rem;
-    > button {
-      text-align: center;
-      font-size: 1rem;
-      opacity: 0.8;
-      background-color: transparent;
-      border: none;
-      margin-left: 3px;
+    border: 1px solid var(--holder-base-color);
+    flex-wrap: wrap;
+    .tag {
+      display: flex;
+      align-items: center;
+      white-space: nowrap;
+      margin: 0 0.5rem 0 1rem;
+      border-radius: var(--radius-10);
       color: var(--font-base-black);
-      cursor: pointer;
-      &:hover {
-        scale: calc(1.3);
-        transition: 0.1s all linear;
+      font-size: 1.5rem;
+      > button {
+        text-align: center;
+        font-size: 1rem;
+        opacity: 0.8;
+        background-color: transparent;
+        border: none;
+        margin-left: 3px;
+        color: var(--font-base-black);
+        cursor: pointer;
+        &:hover {
+          scale: calc(1.3);
+          transition: 0.1s all linear;
+        }
       }
     }
-  }
-  .tag__input--create {
-    border: none;
-    flex: 1;
+    .tag__input--create {
+      border: none;
+      flex: 1;
+      margin-top: 0;
+    }
   }
 `;
 
@@ -191,15 +191,10 @@ const TagWrapper = styled.ul`
 const ButtonContainer = styled.form`
   display: flex;
   justify-content: start;
-  span {
-    font-size: var(--font-15);
-  } /* justify-content: end; 우측 정렬이 나을수도(오른손잡이..) */
 `;
 
+// 등록 버튼 스타일링
 const PublishButton = styled(DefaultButton)`
-  width: 10vw;
-  height: 4vh;
-  font-weight: var(--font-bold);
   @media screen and (max-width: 549px) {
     width: 25vw;
     height: 4vh;
@@ -211,34 +206,36 @@ const PublishButton = styled(DefaultButton)`
   }
 `;
 
+// 취소 버튼 스타일링
 const CancelButton = styled(TransparentButton)`
-  width: 10vw;
-  font-weight: var(--font-bold);
-  @media screen and (max-width: 549px) {
-    width: 28vw;
-    height: 4vh;
-  }
   &:hover {
     color: red;
     transition: 0.1s ease-in-out;
+  }
+  @media screen and (max-width: 549px) {
+    width: 28vw;
+    height: 4vh;
   }
 `;
 
 function PublishForm() {
   const navigate = useNavigate();
-
   const [title, setTitle] = useState();
   const [content, setContent] = useState();
   const [location, setLocation] = useState();
+  /* eslint-disable */
   const [category, setCategory] = useState();
   const [isActive, setIsActive] = useState(false);
   const [tags, setTags] = useState([]);
   const [newTag, setNewTag] = useState();
+  const [confirmModalOpened, setConfirmModalOpened] = useState(false);
+  const [yesNoModalOpened, setYesNoModalOpened] = useState(false);
 
   const MIN_LENGTH = 5;
   const [titleLength, setTitleLength] = useState();
   const [contentLength, setContentLength] = useState();
 
+  // 테마 선택하기
   const selectHandler = (item, index) => {
     setIsActive(index);
     setTimeout(() => {
@@ -246,6 +243,7 @@ function PublishForm() {
     }, 0);
   };
 
+  // 해쉬태그 추가하기
   const handleTagAdd = event => {
     const { value } = event.target;
     const regex = /^[ㄱ-ㅎ|가-힣|a-z|A-Z|0-9|]+$/;
@@ -263,13 +261,46 @@ function PublishForm() {
     }
   };
 
+  // 해쉬태그 삭제하기
   const handleTagRemove = i => {
     setTags([...tags.filter((_, index) => index !== i)]);
   };
 
-  const handleSubmit = async event => {
-    event.prevent.Default();
+  // 등록 버튼 모달연결
+  const confirmModalOpener = event => {
+    event.preventDefault();
+    setConfirmModalOpened(true);
   };
+
+  const confirmModalCloser = () => {
+    setConfirmModalOpened(false);
+    navigate(-1);
+  };
+
+  // 취소 버튼 모달연결
+  const yesNoModalOpener = event => {
+    event.preventDefault();
+    setYesNoModalOpened(true);
+  };
+
+  const yesNoModalActioner = () => {
+    navigate(-1);
+  };
+
+  const yesNoModalCloser = () => {
+    setYesNoModalOpened(false);
+  };
+
+  // 확인버튼 1.5초 후 창닫기
+  useEffect(() => {
+    let timer;
+    if (confirmModalOpened) {
+      timer = setTimeout(() => {
+        confirmModalCloser();
+      }, 1500);
+    }
+    return () => clearTimeout(timer);
+  }, [confirmModalOpened]);
 
   return (
     <Container>
@@ -297,7 +328,6 @@ function PublishForm() {
           required
         />
       </TitleContainer>
-
       <ContentContainer>
         <div className="content__label">
           <label htmlFor="content">스토리 공유</label>
@@ -320,7 +350,6 @@ function PublishForm() {
           required
         />
       </ContentContainer>
-
       <LocationCategoryRow>
         <LocationContainer>
           <label className="location__label" htmlFor="location">
@@ -337,10 +366,9 @@ function PublishForm() {
             placeholder="위치를 남겨주세요"
           />
         </LocationContainer>
-
         <CategoryContainer>
           <label htmlFor="category">테마</label>
-          <CategoryWrapper id="category">
+          <div id="category">
             {categoryData.map((item, index) => {
               return (
                 <Category
@@ -355,13 +383,12 @@ function PublishForm() {
                 </Category>
               );
             })}
-          </CategoryWrapper>
+          </div>
         </CategoryContainer>
       </LocationCategoryRow>
-
       <TagContainer>
         <label htmlFor="tag">태그</label>
-        <TagWrapper>
+        <ul className='tag__wrapper'>
           {tags.map((tag, index) => (
             <li className="tag" key={tag}>
               <span className="tag__name">#{tag}</span>
@@ -378,26 +405,40 @@ function PublishForm() {
             onKeyDown={event => handleTagAdd(event)}
             placeholder="# 태그를 입력하세요"
           />
-        </TagWrapper>
+        </ul>
       </TagContainer>
-
       <ButtonContainer>
         <PublishButton
-          onClick={() => {
-            handleSubmit();
-          }}
-          disabled={!category}
+          width="8vw"
+          height="4vh"
+          fontSize="var(--font-15)"
+          fontWeight="var(--font-bold)"
+          onClick={confirmModalOpener}
         >
-          <span>등 록</span>
+          <span>등록</span>
         </PublishButton>
         <CancelButton
-          onClick={() => {
-            navigate(-1);
-          }}
+          width="8vw"
+          fontSize="var(—font-15)"
+          fontWeight="var(—font-bold)"
+          onClick={yesNoModalOpener}
         >
-          <span>취 소</span>
+          <span>취소</span>
         </CancelButton>
       </ButtonContainer>
+      {confirmModalOpened ? (
+        <ConfirmModal
+          modalMessage="게시글 등록이 완료되었습니다."
+          modalCloser={confirmModalCloser}
+        />
+      ) : null}
+      {yesNoModalOpened ? (
+        <YesNoModal
+          modalMessage="게시글 작성을 취소할까요?"
+          modalActioner={yesNoModalActioner}
+          modalCloser={yesNoModalCloser}
+        />
+      ) : null}
     </Container>
   );
 }
