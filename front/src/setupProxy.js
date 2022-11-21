fb2d86f25a942282a0d7dc654e8d143ee4da7214
starplanter93 <starplanter93@gmail.com>
2022-11-21 15:23:20 +0900

    [Feat] 프록시에 /image-files 엔드포인트 추가

--- front/src/setupProxy.js
+++ front/src/setupProxy.js
@@ -3,7 +3,15 @@ const { createProxyMiddleware } = require('http-proxy-middleware');
 module.exports = app => {
   app.use(
     createProxyMiddleware(
-      ['/login', '/accounts', '/follows', '/boards', '/comments', '/likes'],
+      [
+        '/login',
+        '/accounts',
+        '/follows',
+        '/boards',
+        '/comments',
+        '/likes',
+        '/image-files',
+      ],
       {
         target: process.env.REACT_APP_BACKEND,
         changeOrigin: true,
