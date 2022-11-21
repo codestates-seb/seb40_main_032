6a2f4c3c9b83c75ce1cb3a272d1eee409119028d
starplanter93 <starplanter93@gmail.com>
2022-11-21 19:30:39 +0900

    [Docs] loginUserApi 파일 내 필요없는 주석 제거

--- front/src/api/loginUserApi.js
+++ front/src/api/loginUserApi.js
@@ -1,16 +1,6 @@
 import axios from 'axios';
 import { getCookie } from '../util/cookie';
 
-// const accessToken = getCookie('accessToken');
-
-// const loginUserApi = async () => {
-//   return axios.get('/accounts/login', {
-//     headers: { Authorizaiotn: accessToken },
-//   });
-// };
-
-// export default loginUserApi;
-
 const loginUserApi = async () => {
   const jwt = getCookie('accessToken');
   const login = await axios.get('/accounts/login', {
