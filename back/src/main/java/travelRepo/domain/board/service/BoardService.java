bb65afda4168ac8d45d5caa7a6d72f2b44d97d1a
Cutaku <107826896+Cutaku@users.noreply.github.com>
2022-11-21 19:32:06 +0900

    [Fix] 사용되지 않는 import문 삭제

--- back/src/main/java/travelRepo/domain/board/service/BoardService.java
+++ back/src/main/java/travelRepo/domain/board/service/BoardService.java
@@ -6,7 +6,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.multipart.MultipartFile;
 import travelRepo.domain.account.entity.Account;
 import travelRepo.domain.account.repository.AccountRepository;
 import travelRepo.domain.board.dto.BoardAddReq;
