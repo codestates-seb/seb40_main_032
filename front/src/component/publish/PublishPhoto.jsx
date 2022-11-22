63d6f48dce1e59489647aceb040686fbe9004905
yoosmg <sryu2s@gmail.com>
2022-11-22 20:58:02 +0900

    [Feat] 게시글 작성 사진미리보기 개별 삭제까지 구현

--- front/src/component/publish/PublishPhoto.jsx
+++ front/src/component/publish/PublishPhoto.jsx
@@ -1,8 +1,8 @@
 import { useEffect, useState } from 'react';
 import styled from 'styled-components';
 import { BsCamera } from 'react-icons/bs';
-import { AiOutlineDelete } from 'react-icons/ai';
 import uuid from 'react-uuid';
+import { IoIosRemoveCircleOutline } from 'react-icons/io';
 import postPhotoApi from '../../api/postPhotoApi';
 
 const Container = styled.section`
@@ -15,6 +15,9 @@ const Container = styled.section`
     width: 100%;
     height: 100%;
     flex-wrap: nowrap;
+    > div {
+      display: flex;
+    }
   }
 
   .uploader__default {
@@ -25,12 +28,22 @@ const Container = styled.section`
     border-radius: 1rem;
   }
 
+  #uploader__preview {
+    position: relative;
+  }
+
   .uploader__preview {
     background-size: cover;
     width: 10rem;
     height: 10rem;
     border-radius: 1rem;
-    margin-right: 1.5rem;
+  }
+
+  .remove__button {
+    position: absolute;
+    top: 0;
+    right: 0;
+    font-weight: var(--font-semi-bold);
   }
 
   .upload__button--wrapper {
@@ -75,38 +88,42 @@ const CameraIcon = styled(BsCamera)`
   font-size: 3rem;
 `;
 
-function PublishPhoto() {
+const RemoveIcon = styled(IoIosRemoveCircleOutline)`
+  font-size: 1.5rem;
+  &:hover {
+    color: red;
+  }
+`;
+
+function PublishPhoto({ setPhotoUrl }) {
   const [photos, setPhotos] = useState([]);
   const [blobPhotos, setBlobPhotos] = useState();
 
   // 미리보기 사진 띄우기 - 클라이언트 메모리로 처리
   const uploadPhotos = async event => {
     const uploaded = [...photos];
-    console.log(event.target.files);
     setBlobPhotos(...event.target.files);
     const targetFiles = [...event.target.files]; // 등록 사진
     targetFiles.map(obj => {
-      return uploaded.push(URL.createObjectURL(obj)); // Blob 객체를 URL로 바꾸어 img src로 mapping
+      return uploaded.push(URL.createObjectURL(obj)); // Blob 객체를 임시 URL로 바꾸어 img src로 mapping
     });
     setPhotos(uploaded);
   };
-  // 미리보기 사진 삭제
-  const deletePhotos = async () => {
-    URL.revokeObjectURL(photos);
-    setPhotos([]);
+
+  /* eslint-disable */
+  const handleClick = event => {
+    event.target.value = ''; // 동일한 사진을 여러번 올릴수 있게 해줌
   };
 
   // blob에 사진 추가 시 사진 post 요청
-  console.log(blobPhotos);
   useEffect(() => {
     const formData = new FormData();
     if (blobPhotos) {
       formData.append(`images`, blobPhotos, `${blobPhotos.name}`);
-      console.log(formData.get('images'));
     }
     try {
       if (blobPhotos) {
-        postPhotoApi(formData).then(res => console.log(res.data.imagePaths));
+        postPhotoApi(formData).then(res => setPhotoUrl(...res.data.imagePaths));
       }
     } catch (err) {
       console.log(err);
@@ -128,13 +145,14 @@ function PublishPhoto() {
           <div className="uploader__default" />
         ) : (
           <div>
-            {photos.map(url => (
-              <img
-                src={url}
-                alt="photos"
-                className="uploader__preview"
-                key={uuid()}
-              />
+            {photos.map((url, index) => (
+              <div id="uploader__preview" key={uuid()}>
+                <img src={url} alt="photos" className="uploader__preview" />
+                <RemoveIcon
+                  className="remove__button"
+                  onClick={() => removePhotos(index)}
+                />
+              </div>
             ))}
           </div>
         )}
@@ -150,6 +168,7 @@ function PublishPhoto() {
               id="upload__button"
               type="file"
               onChange={uploadPhotos}
+              onClick={handleClick}
               accept="image/jpg, image/png, image/jpeg"
             />
           </div>
@@ -159,7 +178,6 @@ function PublishPhoto() {
         <span className="footer">
           업로드 가능한 파일 포맷은 jpg, jpeg, png입니다.
         </span>
-        <AiOutlineDelete className="delete__button" onClick={deletePhotos} />
       </div>
     </Container>
   );
