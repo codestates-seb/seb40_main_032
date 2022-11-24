1c0a3780d7c4f10bf11afac0812ebd89068c79c7
seungmin2ee <tmdals2815@gmail.com>
2022-11-25 01:00:04 +0900

    [Refactor] threshold 옵션 추가

--- front/src/hooks/useIntersect.js
+++ front/src/hooks/useIntersect.js
@@ -1,13 +1,14 @@
 import axios from 'axios';
 import { useCallback, useEffect, useRef, useState } from 'react';
 
-// 사용법 : 반환되는 target을 옵저버의 target으로 하고자하는 요소의 ref 값에 할당
+// 사용법: 반환되는 target을 옵저버의 target으로 하고자하는 요소의 ref 값에 할당
 // 파라미터 설명
 // api: 슬래시(/)부터 size 전까지의 특수문자(& or ?)까지 모두 포함
 // size: 요청당 원하는 데이터 개수
 // setPosts: 받아온 데이터를 state값으로 바꿔줄 setState함수
 // setIsPending: pending state를 바꿔줄 setState함수
-const useIntersect = (api, size, setPosts, setIsPending) => {
+// threshold(default는 0): 지정한 root(default는 뷰포트)에 target이 얼마나 보이는지 비율(0~1)
+const useIntersect = (api, size, setPosts, setIsPending, threshold = 0) => {
   const target = useRef(null);
   const page = useRef(1);
   const [hasNextPage, SetHasNextPage] = useState(true);
@@ -28,11 +29,14 @@ const useIntersect = (api, size, setPosts, setIsPending) => {
   }, []);
 
   useEffect(() => {
-    const io = new IntersectionObserver(([entry]) => {
-      if (entry.isIntersecting) {
-        getData();
-      }
-    });
+    const io = new IntersectionObserver(
+      ([entry]) => {
+        if (entry.isIntersecting) {
+          getData();
+        }
+      },
+      { threshold },
+    );
     if (target.current && hasNextPage) {
       io.observe(target.current);
     }
