4d02a0843221c170937efa6a19b82f186b26f0c6
starplanter93 <starplanter93@gmail.com>
2022-11-25 15:31:10 +0900

    [Feat] 내정보 - 좋아요 페이지 데이터 렌더링

--- front/src/pages/myPages/MyLikes.jsx
+++ front/src/pages/myPages/MyLikes.jsx
@@ -1,12 +1,65 @@
-import React from 'react';
+import React, { useState } from 'react';
 import styled from 'styled-components';
+import Post from '../../component/common/Post';
+import useIntersect from '../../hooks/useIntersect';
+import { getCookie } from '../../util/cookie';
 
 const MyPageMain = styled.main`
-  padding-top: 20rem;
+  padding-top: 23rem;
+  max-width: 172rem;
+  margin: 0 3rem;
+  display: flex;
+  flex-wrap: wrap;
+
+  @media screen and (max-width: 1440px) {
+    .post,
+    .post__skeleton {
+      flex-basis: 25%;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    .post,
+    .post__skeleton {
+      flex-basis: 33.3%;
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .post,
+    .post__skeleton {
+      flex-basis: 50%;
+    }
+  }
+
+  @media screen and (max-width: 549px) {
+    margin: 0 1rem;
+    .post,
+    .post__skeleton {
+      flex-basis: 100%;
+    }
+  }
 `;
 
 function MyLikes() {
-  return <MyPageMain>MyLikes</MyPageMain>;
+  const [myLike, setMyLike] = useState([]);
+  const [isPending, setIsPending] = useState(false);
+  const accountId = getCookie('accountId');
+  const target = useIntersect(
+    `/boards/like/account/${accountId}?`,
+    5,
+    setMyLike,
+    setIsPending,
+  );
+  return (
+    <MyPageMain>
+      {myLike.map(like => {
+        return <Post key={like.boardId} post={like} />;
+      })}
+      {isPending && <div>로딩중...</div>}
+      <div ref={target} className="target" />
+    </MyPageMain>
+  );
 }
 
 export default MyLikes;
