57482cc179ec2ca33a99a966f15612078819e48e
starplanter93 <starplanter93@gmail.com>
2022-11-25 19:31:54 +0900

    [Feat] MyFollower와 MyFollowing에 데이터 내려주기

--- front/src/pages/myPages/MyFollower.jsx
+++ front/src/pages/myPages/MyFollower.jsx
@@ -1,11 +1,19 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import styled from 'styled-components';
+import followDataApi from '../../api/followDataApi';
 
 const MyPageMain = styled.main`
-  padding-top: 20rem;
+  padding-top: 23rem;
+  max-width: 172rem;
+  margin: 0 3rem;
 `;
 
 function MyFollower() {
+  useEffect(() => {
+    followDataApi(1, 'follower')
+      .then(res => console.log(res))
+      .catch(err => console.log(err));
+  }, []);
   return <MyPageMain>MyFollower</MyPageMain>;
 }
 
