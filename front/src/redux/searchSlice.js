bf46c5c6b0bc911f55a89808689c156f595a3a14
kuku <covernat6949@gmail.com>
2022-11-27 02:19:12 +0900

    [Feat] 이전 페이지의 정렬을 기억하기 위한 slice값 추가

--- front/src/redux/searchSlice.js
+++ front/src/redux/searchSlice.js
@@ -4,10 +4,18 @@ const searchSlice = createSlice({
   name: 'search',
   initialState: {
     search: '',
+    memorySort: 'createdAt,desc',
+    path: '/',
   },
   reducers: {
     setSearch(state, { payload }) {
       state.search = payload;
+    },
+    setSort(state, { payload }) {
+      state.memorySort = payload;
+    },
+    setPath(state, { payload }) {
+      state.path = payload;
     },
   },
 });
