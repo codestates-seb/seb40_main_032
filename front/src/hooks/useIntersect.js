a015ec018b81d90acb57367c7dd7bd1a193b87db
kuku <covernat6949@gmail.com>
2022-11-28 19:26:19 +0900

    [Feat] Api 파라미터 변경으로 인한 useIntersect 수정

--- front/src/hooks/useIntersect.js
+++ front/src/hooks/useIntersect.js
@@ -15,31 +15,51 @@ const useIntersect = (
   setPosts,
   setIsPending,
   threshold = 0,
-  sort = 'createdAt',
+  sort = 'createdAt,desc',
+  keyword = '',
 ) => {
   const target = useRef(null);
   const page = useRef(1);
   const [hasNextPage, SetHasNextPage] = useState(true);
+  const [lastData, setLastData] = useState('');
 
   const getData = useCallback(
     async search => {
       try {
         setIsPending(true);
         console.log(
-          `${api}${search !== '' ? `query=${search}&` : ''}size=${size}&page=${
-            page.current
-          }&sort=${sort}`,
+          // boardId createdAt likecounts views likeboardId
+          `${api}${
+            search !== '' ? `query=${search}&` : ''
+          }size=${size}&sort=${sort}&${lastData}`,
         );
         const { data } = await axios(
-          `${api}${search !== '' ? `query=${search}&` : ''}size=${size}&page=${
-            page.current
-          }&sort=${sort}`,
+          `${api}${
+            search !== '' ? `query=${search}&` : ''
+          }size=${size}&sort=${sort}&${lastData}`,
         );
         setPosts(prev => [...prev, ...data.content]);
         setIsPending(false);
         SetHasNextPage(data.hasNext);
         if (data.hasNext) {
-          page.current += 1;
+          const last = data.content[data.content.length - 1];
+          if (keyword === 'like') {
+            setLastData(
+              `lastLikeId=${last.likeId}&lastLikeCreatedAt=${last.likeCreatedAt}`,
+            );
+          } else if (sort === 'createdAt,desc') {
+            setLastData(
+              `lastBoardId=${last.boardId}&lastBoardCreatedAt=${last.createdAt}`,
+            );
+          } else if (sort === 'likeCount,desc') {
+            setLastData(
+              `lastBoardId=${last.boardId}&lastBoardLikeCount=${last.likeCount}`,
+            );
+          } else if (sort === 'views,desc') {
+            setLastData(
+              `lastBoardId=${last.boardId}&lastBoardViews=${last.views}`,
+            );
+          }
         } else {
           target.current.style.display = 'none';
         }
@@ -47,7 +67,7 @@ const useIntersect = (
         console.log('Error', err.message);
       }
     },
-    [sort],
+    [sort, lastData],
   );
 
   useEffect(() => {
@@ -64,7 +84,7 @@ const useIntersect = (
     }
     console.log('3');
     return () => io.disconnect();
-  }, [hasNextPage, sort, query]);
+  }, [hasNextPage, sort, query, lastData]);
 
   return [target, page, SetHasNextPage];
 };
