e3f9c8bea4027e327082b0a62c2b63c1dfb84467
starplanter93 <starplanter93@gmail.com>
2022-11-28 15:51:48 +0900

    [Layout] 컴포넌트 분리에 따른 css위치 수정

--- front/src/component/follow/FollowList.jsx
+++ front/src/component/follow/FollowList.jsx
@@ -1,5 +1,4 @@
 import styled from 'styled-components';
-
 import FollowPost from './FollowPost';
 import FollowUserCard from './FollowUserCard';
 
@@ -20,45 +19,6 @@ const FollowListContainer = styled.div`
   }
   &:last-child::after {
     display: none;
-  }
-
-  .followList__leftside {
-    padding: 0 5rem 0 1.2rem;
-
-    @media screen and (max-width: 549px) {
-      padding-right: 1.2rem;
-    }
-    .followList__userinfo {
-      display: flex;
-      flex-direction: column;
-      align-items: center;
-      padding: 5rem 0;
-
-      .followList__avatar {
-        width: 12rem;
-        height: 12rem;
-        border-radius: 50%;
-        background: #eee;
-        overflow: hidden;
-        > img {
-          width: 100%;
-          height: 100%;
-        }
-      }
-
-      .followList__username {
-        margin: 1rem 0 2rem;
-        > p {
-          font-size: 1.4rem;
-          overflow: hidden;
-          text-overflow: ellipsis;
-          display: -webkit-box;
-          -webkit-line-clamp: 1;
-          -webkit-box-orient: vertical;
-          word-break: break-all;
-        }
-      }
-    }
   }
 
   .followList__rightside {
--- front/src/component/follow/FollowUserCard.jsx
+++ front/src/component/follow/FollowUserCard.jsx
@@ -1,9 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
 import { DefaultButton, NegativeButton } from '../common/button/ButtonStyle';
+
+const FollowListLeftSide = styled.div`
+  padding: 0 5rem 0 1.2rem;
+
+  @media screen and (max-width: 549px) {
+    padding-right: 1.2rem;
+  }
+  .followList__userinfo {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    padding: 5rem 0;
+
+    .followList__avatar {
+      width: 12rem;
+      height: 12rem;
+      border-radius: 50%;
+      background: #eee;
+      overflow: hidden;
+      > img {
+        width: 100%;
+        height: 100%;
+      }
+    }
+
+    .followList__username {
+      margin: 1rem 0 2rem;
+      > p {
+        font-size: 1.4rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        display: -webkit-box;
+        -webkit-line-clamp: 1;
+        -webkit-box-orient: vertical;
+        word-break: break-all;
+      }
+    }
+  }
+`;
 
 function FollowUserCard({ myFollowing }) {
   return (
-    <div className="followList__leftside">
+    <FollowListLeftSide>
       <ul className="followList__userinfo">
         <li className="followList__avatar">
           <img src={myFollowing.profile} alt="아바타" />
@@ -23,7 +63,7 @@ function FollowUserCard({ myFollowing }) {
           )}
         </li>
       </ul>
-    </div>
+    </FollowListLeftSide>
   );
 }
 
