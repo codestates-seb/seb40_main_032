ec2ec277ce9169747b3d0cc9fe378c9702b90e72
starplanter93 <starplanter93@gmail.com>
2022-11-28 15:30:38 +0900

    [Layout] 내정보 팔로잉탭 padding-top수정

--- front/src/pages/myPages/MyFollowing.jsx
+++ front/src/pages/myPages/MyFollowing.jsx
@@ -4,7 +4,7 @@ import followDataApi from '../../api/followDataApi';
 import FollowList from '../../component/follow/FollowList';
 
 const MyPageMain = styled.main`
-  padding-top: 23rem;
+  padding-top: 20rem;
   max-width: 172rem;
   margin: 0 3rem;
   @media screen and (max-width: 549px) {
