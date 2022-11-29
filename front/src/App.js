27ab1c30347cf446c34b8840cd2984932f28e62d
DT-KUKU <107905739+DT-KUKU@users.noreply.github.com>
2022-11-29 18:04:39 +0900

    [Feat] GA 적용 #261(#275)
    * [Feat] react-ga history 추가
    
    * [Feat] GA 페이지 감지 및 이벤트 감지 추가
    
    * [Feat] GA 페이지 감지에 따른 Router 위치 조정
    
    * [Feat] 로그아웃에 GA 이벤트 감지 등록
    
    * [Refoctor] GA 디버그 OFF

--- front/src/App.js
+++ front/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import React, { Suspense } from 'react';
-import { Route, Routes, BrowserRouter as Router } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import { useSelector } from 'react-redux';
 import { ToastContainer } from 'react-toastify';
 import Loading from './component/common/Loading';
@@ -19,11 +19,13 @@ import MyFollower from './pages/myPages/MyFollower';
 import MyFollowing from './pages/myPages/MyFollowing';
 import MainPage from './pages/mainPages/MainPage';
 import MyInfoEdit from './pages/myPages/MyInfoEdit';
+import PageTracking from './util/PageGaTracking';
 
 const PublishPage = React.lazy(() => import('./pages/PublishPage'));
 const PostDetailPage = React.lazy(() => import('./pages/PostDetailPage'));
 
 function App() {
+  PageTracking();
   const loginModalOpened = useSelector(
     state => state.loginModal.loginModalOpened,
   );
@@ -32,31 +34,29 @@ function App() {
   );
 
   return (
-    <Router>
-      <Suspense fallback={<Loading />}>
-        <ToastContainer />
-        {loginModalOpened && <LoginModal />}
-        {signupModalOpened && <SignupModal />}
-        <Header />
-        <Routes>
-          <Route path="/" element={<MainPage />}>
-            <Route index element={<AllThemePage />} />
-            <Route path="restaurant" element={<RestaurantPage />} />
-            <Route path="stay" element={<StayPage />} />
-            <Route path="spot" element={<SpotPage />} />
-          </Route>
-          <Route path="/postDetail/:id" element={<PostDetailPage />} />
-          <Route path="/publish" element={<PublishPage />} />
-          <Route path="/mypage" element={<MyPage />}>
-            <Route index element={<MyPost />} />
-            <Route path="mylikes" element={<MyLikes />} />
-            <Route path="myfollower" element={<MyFollower />} />
-            <Route path="myfollowing" element={<MyFollowing />} />
-          </Route>
-          <Route path="/myinfoedit" element={<MyInfoEdit />} />
-        </Routes>
-      </Suspense>
-    </Router>
+    <Suspense fallback={<Loading />}>
+      <ToastContainer />
+      {loginModalOpened && <LoginModal />}
+      {signupModalOpened && <SignupModal />}
+      <Header />
+      <Routes>
+        <Route path="/" element={<MainPage />}>
+          <Route index element={<AllThemePage />} />
+          <Route path="restaurant" element={<RestaurantPage />} />
+          <Route path="stay" element={<StayPage />} />
+          <Route path="spot" element={<SpotPage />} />
+        </Route>
+        <Route path="/postDetail/:id" element={<PostDetailPage />} />
+        <Route path="/publish" element={<PublishPage />} />
+        <Route path="/mypage" element={<MyPage />}>
+          <Route index element={<MyPost />} />
+          <Route path="mylikes" element={<MyLikes />} />
+          <Route path="myfollower" element={<MyFollower />} />
+          <Route path="myfollowing" element={<MyFollowing />} />
+        </Route>
+        <Route path="/myinfoedit" element={<MyInfoEdit />} />
+      </Routes>
+    </Suspense>
   );
 }
 
