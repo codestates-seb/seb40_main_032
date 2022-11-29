a00ae41415454301422a98a4bb5502ff9c288cf4
kuku <covernat6949@gmail.com>
2022-11-30 01:18:21 +0900

    [Feat] sentry 적용

--- front/src/index.js
+++ front/src/index.js
@@ -3,9 +3,21 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import { Provider } from 'react-redux';
 import { BrowserRouter as Router } from 'react-router-dom';
+import * as Sentry from '@sentry/react';
+import { Integrations } from '@sentry/tracing';
 import store from './redux/store';
 import App from './App';
 import GlobalStyle from './assets/GlobalStyle';
+
+Sentry.init({
+  dsn: process.env.REACT_APP_SENTRY_TRACKING_DNS,
+  integrations: [
+    new Integrations.BrowserTracing({
+      tracingOrigins: process.env.REACT_APP_BACKEND,
+    }),
+  ],
+  tracesSampleRate: 1.0,
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
