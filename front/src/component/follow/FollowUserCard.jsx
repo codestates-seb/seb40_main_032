61a38916c5c853bed86d78f3d4c3b5eb8e041245
seungmin2ee <tmdals2815@gmail.com>
2022-11-30 18:31:41 +0900

    [Refactor] FollowUserCard 반응형 수정 및 추가

--- front/src/component/follow/FollowUserCard.jsx
+++ front/src/component/follow/FollowUserCard.jsx
@@ -4,16 +4,13 @@ import { DefaultButton, NegativeButton } from '../common/button/ButtonStyle';
 import { postDetailFollowApi } from '../../api/postDetailApi';
 
 const FollowListLeftSide = styled.div`
-  padding: 0 5rem 0 1.2rem;
+  padding: 0 4rem 0 2rem;
 
-  @media screen and (max-width: 549px) {
-    padding-right: 1.2rem;
-  }
   .followList__userinfo {
     display: flex;
     flex-direction: column;
     align-items: center;
-    padding: 5rem 0;
+    padding: 4.5rem 0;
 
     .followList__avatar {
       width: 12rem;
@@ -24,6 +21,7 @@ const FollowListLeftSide = styled.div`
       > img {
         width: 100%;
         height: 100%;
+        object-fit: cover;
       }
     }
 
@@ -37,6 +35,65 @@ const FollowListLeftSide = styled.div`
         -webkit-line-clamp: 1;
         -webkit-box-orient: vertical;
         word-break: break-all;
+      }
+    }
+
+    .followList__button {
+      > button {
+        font-size: 1.4rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 1440px) {
+    .followList__userinfo {
+      padding: 3.5rem 0;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    .followList__userinfo {
+      .followList__avatar {
+        width: 10rem;
+        height: 10rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    .followList__userinfo {
+      .followList__avatar {
+        width: 9rem;
+        height: 9rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 549px) {
+    width: 100%;
+    padding: 0 1.5rem;
+    .followList__userinfo {
+      width: 100%;
+      flex-direction: row;
+      padding: 0;
+      .followList__avatar {
+        width: 6rem;
+        height: 6rem;
+      }
+      .followList__username {
+        padding-left: 1rem;
+        margin: 0;
+      }
+
+      .followList__button {
+        flex-grow: 2;
+        display: flex;
+        justify-content: flex-end;
+        > button {
+          width: 7rem;
+          height: 2.5rem;
+          font-size: 1.2rem;
+        }
       }
     }
   }
@@ -66,7 +123,7 @@ function FollowUserCard({ myFollowing }) {
             </DefaultButton>
           ) : (
             <NegativeButton width="9rem" height="3rem" onClick={followHandler}>
-              언팔로우
+              팔로우중
             </NegativeButton>
           )}
         </li>
