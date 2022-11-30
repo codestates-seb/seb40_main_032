71e6d8acfe3734ec36367cc096beaf0ead1626ec
seungmin2ee <tmdals2815@gmail.com>
2022-11-30 18:30:18 +0900

    [Refactor] FollowPost 반응형 수정

--- front/src/component/follow/FollowPost.jsx
+++ front/src/component/follow/FollowPost.jsx
@@ -4,35 +4,6 @@ const FollowThumb = styled.li`
   flex-basis: 16.6%;
   padding: 0 1.2rem;
 
-  @media screen and (max-width: 1440px) {
-    flex-basis: 25%;
-    &:nth-child(5) {
-      display: none;
-    }
-  }
-
-  @media screen and (max-width: 1024px) {
-    flex-basis: 33.3%;
-    &:nth-child(4) {
-      display: none;
-    }
-  }
-
-  @media screen and (max-width: 768px) {
-    flex-basis: 50%;
-    &:nth-child(3) {
-      display: none;
-    }
-  }
-
-  @media screen and (max-width: 549px) {
-    margin: 0 1rem;
-    flex-basis: 100%;
-    &:nth-child(2) {
-      display: none;
-    }
-  }
-
   .FollowThumb__container {
     display: flex;
     flex-direction: column;
@@ -66,6 +37,32 @@ const FollowThumb = styled.li`
       -webkit-line-clamp: 2;
       -webkit-box-orient: vertical;
       word-break: break-all;
+    }
+  }
+
+  @media screen and (max-width: 1440px) {
+    flex-basis: 20%;
+    &:nth-child(6) {
+      display: none;
+    }
+  }
+
+  @media screen and (max-width: 1024px) {
+    flex-basis: 25%;
+    &:nth-child(5) {
+      display: none;
+    }
+    .followThumb__title {
+      > p {
+        font-size: 1.5rem;
+      }
+    }
+  }
+
+  @media screen and (max-width: 768px) {
+    flex-basis: 33.3%;
+    &:nth-child(4) {
+      display: none;
     }
   }
 `;
