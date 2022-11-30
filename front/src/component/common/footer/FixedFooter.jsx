e6c6aeb3590c973675d7cbc8d0dfbf57f9779a90
seungmin2ee <tmdals2815@gmail.com>
2022-12-01 04:35:27 +0900

    [Refactor] Footer와 FixedFooter에 CommonFooter 적용

--- front/src/component/common/footer/FixedFooter.jsx
+++ front/src/component/common/footer/FixedFooter.jsx
@@ -1,47 +1,16 @@
 import styled from 'styled-components';
-import { AiFillGithub } from 'react-icons/ai';
+import CommonFooter from './CommonFooter';
 
 const FooterWrapper = styled.footer`
   display: flex;
   justify-content: center;
   width: 100%;
-  height: 5rem;
   box-shadow: 0 0 6px rgb(0 0 0 / 16%);
   position: fixed;
   bottom: 0px;
   left: 0px;
   background-color: hsl(0, 0%, 100%);
-  .footer__container {
-    width: 100%;
-    max-width: 172rem;
-    margin: 0 4rem;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
 
-    > div {
-      display: flex;
-      .footer__logo {
-        font-family: 'Lobster', cursive;
-        color: var(--button-theme);
-        font-size: 1.8rem;
-      }
-      .copyright {
-        font-size: 1.4rem;
-        color: var(--font-base-grey);
-        padding: 4px 0 0 1rem;
-      }
-    }
-    .github {
-      > a {
-        color: var(--font-base-black);
-      }
-      > a > svg {
-        width: 3rem;
-        height: 3rem;
-      }
-    }
-  }
   @media screen and (max-width: 549px) {
     display: none;
   }
@@ -50,22 +19,7 @@ const FooterWrapper = styled.footer`
 function Footer() {
   return (
     <FooterWrapper>
-      <div className="footer__container">
-        <div>
-          <p className="footer__logo">Tripagram</p>
-          <p className="copyright">
-            Copyright 2022. Tripagram. All rights reserved.
-          </p>
-        </div>
-        <div className="github">
-          <a
-            href="https://github.com/codestates-seb/seb40_main_032"
-            target="blank"
-          >
-            <AiFillGithub />
-          </a>
-        </div>
-      </div>
+      <CommonFooter />
     </FooterWrapper>
   );
 }
--- front/src/component/common/footer/Footer.jsx
+++ front/src/component/common/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import styled from 'styled-components';
-import { AiFillGithub } from 'react-icons/ai';
+import CommonFooter from './CommonFooter';
 
 const FooterWrapper = styled.footer`
   flex: 1;
@@ -16,36 +16,7 @@ const FooterWrapper = styled.footer`
     background: #f7f7f7;
 
     .footer__container {
-      width: 100%;
-      max-width: 172rem;
       height: 6rem;
-      margin: 0 4rem;
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-
-      > div {
-        display: flex;
-        .footer__logo {
-          font-family: 'Lobster', cursive;
-          color: var(--button-theme);
-          font-size: 1.8rem;
-        }
-        .copyright {
-          font-size: 1.4rem;
-          color: var(--font-base-grey);
-          padding: 4px 0 0 1rem;
-        }
-      }
-      .github {
-        > a {
-          color: var(--font-base-black);
-        }
-        > a > svg {
-          width: 3rem;
-          height: 3rem;
-        }
-      }
     }
   }
 `;
@@ -54,22 +25,7 @@ function Footer() {
   return (
     <FooterWrapper>
       <div className="footer__wrapper">
-        <div className="footer__container">
-          <div>
-            <p className="footer__logo">Tripagram</p>
-            <p className="copyright">
-              Copyright 2022. Tripagram. All rights reserved.
-            </p>
-          </div>
-          <div className="github">
-            <a
-              href="https://github.com/codestates-seb/seb40_main_032"
-              target="blank"
-            >
-              <AiFillGithub />
-            </a>
-          </div>
-        </div>
+        <CommonFooter />
       </div>
     </FooterWrapper>
   );
