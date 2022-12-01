7fc28a7e7176f00ed4b85ab26bab3285cf2b50eb
starplanter93 <starplanter93@gmail.com>
2022-12-01 14:15:28 +0900

    [Fix] 임시비밀번호 발급 버그 수정

--- front/src/api/tempPasswordApi.js
+++ front/src/api/tempPasswordApi.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const tempPasswordApi = async email => {
-  const tempPassword = await axios.post('accounts/tempPassword/email', {
+  const tempPassword = await axios.post('/accounts/tempPassword/email', {
     email,
   });
 
