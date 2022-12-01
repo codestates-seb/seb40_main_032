b085b29031f9725b5219c533a432bfab782c3a09
kuku <covernat6949@gmail.com>
2022-12-01 13:49:10 +0900

    [Feat] 수정 페이지 메타태그 적용

--- front/src/pages/PublishPage.jsx
+++ front/src/pages/PublishPage.jsx
@@ -1,5 +1,6 @@
 import styled from 'styled-components';
 import PublishForm from '../component/publish/PublishForm';
+import MetaTag from '../util/MetaTag';
 
 const Publish = styled.main`
   padding: 8rem 20vw;
@@ -11,6 +12,11 @@ const Publish = styled.main`
 function PublishPage() {
   return (
     <Publish>
+      <MetaTag
+        title="나만의 여행 정보 공유 - Tripagram"
+        description="자신만이 알고 있는 여행 장소, 음식점, 숙소에 대해서 공유해주세요"
+        keywords="여행, 숙소, 장소, 공유, 등록"
+      />
       <PublishForm />
     </Publish>
   );
