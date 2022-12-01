508af70d90527a4e9e311a868042e346dd662cb3
kuku <covernat6949@gmail.com>
2022-12-01 13:49:33 +0900

    [Feat] 마이페이지 메타 태그 적용

--- front/src/pages/myPages/MyPage.jsx
+++ front/src/pages/myPages/MyPage.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { Outlet } from 'react-router-dom';
 import styled from 'styled-components';
 import UserInfo from '../../component/userInfo/UserInfo';
+import MetaTag from '../../util/MetaTag';
 
 const MyPageWrapper = styled.div`
   display: flex;
@@ -13,6 +14,11 @@ const MyPageWrapper = styled.div`
 function MyPage() {
   return (
     <MyPageWrapper>
+      <MetaTag
+        title="마이페이지 - Tripagram"
+        description="계정관리와 팔로워 와 팔로잉 좋아요를 관리하는 페이지입니다."
+        keywords="계정관리, 정보수정, 팔로워 및 팔로잉 관리, 게시글 관리, 좋아요 관리"
+      />
       <UserInfo />
       <Outlet />
     </MyPageWrapper>
