d23863c3941eba233d5b041240f349c95e4da453
seungmin2ee <tmdals2815@gmail.com>
2022-12-01 14:56:44 +0900

    [Feat] 팔로워, 팔로잉 유저의 게시글이 없을 경우 문구 적용

--- front/src/component/follow/FollowList.jsx
+++ front/src/component/follow/FollowList.jsx
@@ -1,4 +1,5 @@
 import styled from 'styled-components';
+import { BsCamera } from 'react-icons/bs';
 import FollowPost from './FollowPost';
 import FollowUserCard from './FollowUserCard';
 
@@ -25,6 +26,32 @@ const FollowListContainer = styled.div`
     flex: 1;
     > ul {
       display: flex;
+
+      .followList__empty {
+        width: 100%;
+        display: flex;
+        justify-content: center;
+        > div {
+          display: flex;
+          font-size: 3rem;
+          color: var(--holder-base-color);
+          > span {
+            position: relative;
+            display: block;
+            width: 5rem;
+            height: 5rem;
+            border-radius: 50%;
+            border: 1px solid var(--holder-base-color);
+            margin-right: 1rem;
+            > svg {
+              position: absolute;
+              top: 50%;
+              left: 50%;
+              transform: translate(-50%, -50%);
+            }
+          }
+        }
+      }
     }
   }
 
@@ -40,15 +67,26 @@ const FollowListContainer = styled.div`
 `;
 
 function FollowList({ myFollowing }) {
+  console.log(myFollowing.boards);
   return (
     <FollowListContainer>
       <FollowUserCard myFollowing={myFollowing} />
       <div className="followList__rightside">
         <ul className="followList__posts">
-          {myFollowing &&
+          {myFollowing.boards.length !== 0 ? (
             myFollowing.boards.map(board => (
               <FollowPost key={board.id} boards={board} />
-            ))}
+            ))
+          ) : (
+            <li className="followList__empty">
+              <div>
+                <span>
+                  <BsCamera />
+                </span>
+                <p>스토리가 없습니다.</p>
+              </div>
+            </li>
+          )}
         </ul>
       </div>
     </FollowListContainer>
