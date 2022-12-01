1c43266865ecaf4f8a4ab114319ea6a407403264
seungmin2ee <tmdals2815@gmail.com>
2022-12-01 20:17:05 +0900

    [Feat] 팔로워 데이터가 없을 경우 표시할 텍스트 적용

--- front/src/pages/myPages/MyFollower.jsx
+++ front/src/pages/myPages/MyFollower.jsx
@@ -1,10 +1,12 @@
 import React, { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
 import styled from 'styled-components';
+import { RiUserFollowLine } from 'react-icons/ri';
 import FollowList from '../../component/follow/FollowList';
 import followDataApi from '../../api/followDataApi';
 import Pagination from '../../component/common/Pagination';
 import LoadingSpinner from '../../component/common/LoadingSpinner';
+import EmptyText from '../../component/common/EmptyText';
 
 const MyPageMain = styled.main`
   padding-top: 22rem;
@@ -16,18 +18,40 @@ const MyPageMain = styled.main`
   .follower__container {
     width: 100%;
     max-width: 172rem;
-  }
 
-  .loading__container {
-    width: 100%;
-    display: flex;
-    justify-content: center;
-    padding-top: 1rem;
+    .loading__container {
+      width: 100%;
+      display: flex;
+      justify-content: center;
+      padding-top: 1rem;
+    }
+
+    .empty__follower {
+      width: 100%;
+      height: calc(100vh - 8rem - 22rem - 3rem - 10.1rem);
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      > div {
+        padding-bottom: 3rem;
+      }
+    }
   }
 
   @media screen and (max-width: 549px) {
     padding-top: 15.5rem;
     margin-bottom: 1rem;
+
+    .empty__follower {
+      height: calc(100vh - 8rem - 16.5rem - 16.6rem);
+      > div {
+        font-size: 2rem;
+        > span {
+          width: 4rem;
+          height: 4rem;
+        }
+      }
+    }
   }
 `;
 
@@ -60,6 +84,14 @@ function MyFollower() {
             myFollowing.map(following => (
               <FollowList key={following.id} myFollowing={following} />
             ))
+          )}
+          {!isPending && myFollowing.length === 0 && (
+            <div className="empty__follower">
+              <EmptyText
+                icon={<RiUserFollowLine />}
+                text="팔로워 회원이 없습니다."
+              />
+            </div>
           )}
         </section>
       </MyPageMain>
