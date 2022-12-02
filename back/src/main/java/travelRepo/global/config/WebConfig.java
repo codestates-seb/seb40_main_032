4e934768fc9f111e70d35e8f3f16010cf7c722de
Cutaku <107826896+Cutaku@users.noreply.github.com>
2022-12-02 19:54:30 +0900

    [Fix] WebConfig 설정 삭제

--- back/src/main/java/travelRepo/global/config/WebConfig.java
+++ back/src/main/java/travelRepo/global/config/WebConfig.java
@@ -1,6 +1,5 @@
 package travelRepo.global.config;
 
-import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import travelRepo.global.argumentresolver.LoginAccountIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -15,12 +14,4 @@ public class WebConfig implements WebMvcConfigurer {
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginAccountIdArgumentResolver());
     }
-
-//    @Override
-//    public void addCorsMappings(CorsRegistry registry) {
-//        registry
-//                .addMapping("/**")
-//                .allowedOrigins("http://seb40-mainproject.s3-website.ap-northeast-2.amazonaws.com")
-//                .allowedMethods("*");
-//    }
 }
