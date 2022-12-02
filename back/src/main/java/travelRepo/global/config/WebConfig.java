e671ae146f2c656034773497f9aac8f4dca4407c
hipopotamus <qkqntjddn@gmail.com>
2022-12-02 14:57:25 +0900

    [Fix] Cors 전역 설정

--- back/src/main/java/travelRepo/global/config/WebConfig.java
+++ back/src/main/java/travelRepo/global/config/WebConfig.java
@@ -1,5 +1,6 @@
 package travelRepo.global.config;
 
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import travelRepo.global.argumentresolver.LoginAccountIdArgumentResolver;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,5 +14,13 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(new LoginAccountIdArgumentResolver());
+    }
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry
+                .addMapping("/**")
+                .allowedOrigins("http://seb40-mainproject.s3-website.ap-northeast-2.amazonaws.com/")
+                .allowedMethods("*");
     }
 }
