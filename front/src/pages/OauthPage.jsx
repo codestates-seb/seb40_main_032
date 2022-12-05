b0435c9aa7833968f318194d497726b8a5da26b6
starplanter93 <starplanter93@gmail.com>
2022-12-05 19:34:18 +0900

    [Fix] 소셜로그인 로직 일부 변경

--- front/src/pages/OauthPage.jsx
+++ front/src/pages/OauthPage.jsx
@@ -1,17 +1,25 @@
 import React, { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
+import { loginActions } from '../redux/loginSlice';
 import loginUserApi from '../api/loginUserApi';
 
 function OauthPage() {
-  useEffect(async () => {
+  const dispatch = useDispatch();
+  const oauthLogin = async () => {
     const url = new URL(window.location.href);
     const accessToken = url.searchParams.get('jwt');
     localStorage.setItem('accessToken', `Bearer ${accessToken}`);
     await loginUserApi()
-      .then(res => console.log(res))
-      .catch(err => console.log(err));
+      .then(() => {
+        dispatch(loginActions.login(true));
+      })
+      .catch(err => alert('로그인 실패', console.log(err)));
     window.location.replace('/');
-    console.log(url, accessToken);
-  }, []);
+  };
+  useEffect(() => {
+    oauthLogin();
+  }, [oauthLogin]);
+
   return <div>인증중입니다.</div>;
 }
 
