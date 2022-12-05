1ab245a20e6c8be47826ee2d903b788619d6f252
seungmin2ee <tmdals2815@gmail.com>
2022-12-05 18:32:39 +0900

    [Feat] 좋아요, 코멘트 개수 UI 스타일 적용

--- front/src/component/common/post/CommentLike.jsx
+++ front/src/component/common/post/CommentLike.jsx
@@ -1,18 +1,38 @@
 import { BiHeart, BiMessageDetail } from 'react-icons/bi';
 import styled from 'styled-components';
 
-const CommentLikeBox = styled.div``;
+const CommentLikeBox = styled.div`
+  position: absolute;
+  bottom: 5px;
+  right: 5px;
+  > ul {
+    display: flex;
+    background: rgba(0, 0, 0, 0.2);
+    border-radius: 5px;
+    > li {
+      display: flex;
+      align-items: center;
+      color: #fff;
+      font-size: 1.6rem;
+      padding: 5px;
+      > span {
+        font-size: 1.4rem;
+        padding-left: 2px;
+      }
+    }
+  }
+`;
 
 function CommentLike({ like, comment }) {
   console.log(like, comment);
   return (
     <CommentLikeBox>
       <ul>
-        <li>
+        <li className="post__like">
           <BiHeart />
           <span>{like}</span>
         </li>
-        <li>
+        <li className="post__comment">
           <BiMessageDetail />
           <span>{comment}</span>
         </li>
